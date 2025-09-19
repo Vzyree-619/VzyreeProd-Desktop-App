3452ce7ecdea688bd5d5e623cf35a7e483ecb7a7
vzyree619 <vzyree619@gmail.com>
2025-09-19 20:42:09 +0500

    added css for the test purposes

--- src/Something.jsx
+++ src/Something.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 const Something = ()=>{
     let [count,setCount ] = useState(0)
@@ -6,9 +7,13 @@ const Something = ()=>{
         setCount(count+1);
     }
 
+  useEffect(()=>{
+    console.log("The data is being received")
+  },[])
 
     return <>
     <h1>Findo Trip Dashboard</h1>
+    <div style={{height:"200px",width: "80vw", backgroundColor:"orange",border: "2px solid black"}}></div>
     <button onClick={handleClick}>Increment</button>
     <p>The Value is {count}</p>
      <input type="file" accept="video/*" />
